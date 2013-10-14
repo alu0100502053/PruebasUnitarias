35591d114679807997e873dd40f91481a2a02201
Killian Jimenez <alu0100502053@ull.edu.es>
2013-10-15 00:26:09 +0100

    Arreglado gcd.rb y terminado racional.rb

--- racional.rb
+++ racional.rb
@@ -3,4 +3,72 @@
 require "./gcd.rb"
 
 class Fraccion
+	attr_accessor :n, :d
+
+	def initialize(n,d)
+		@n, @d = n, d
+	end
+
+   def to_s
+      "#{@n}/#{@d}"
+   end
+
+   def reducir(n,d)
+   	mcd = gcd(n,d)
+   	Fraccion.new(n/mcd, d/mcd)
+   end
+
+   def suma(other)
+   	f1 = reducir(@n,@d)
+   	f2 = reducir(other.n, other.d)
+   	if f1.d != f2.d then
+   		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
+   		f1.n = mcm / f1.d * f1.n
+   		f1.d = mcm
+   		f2.n = mcm / f2.d * f2.n
+   		f2.d = mcm
+   	end
+   	ff = Fraccion.new(f1.n + f2.n, f1.d)
+   	reducir(ff.n,ff.d)
+   end
+
+   def resta(other)
+   	f1 = reducir(@n,@d)
+   	f2 = reducir(other.n, other.d)
+   	if f1.d != f2.d then
+   		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
+   		f1.n = mcm / f1.d * f1.n
+   		f1.d = mcm
+   		f2.n = mcm / f2.d * f2.n
+   		f2.d = mcm
+   	end
+   	ff = Fraccion.new(f1.n - f2.n, f1.d)
+   	reducir(ff.n,ff.d)
+   end
+
+   def producto(other)
+      f1 = reducir(@n,@d)
+   	f2 = reducir(other.n, other.d)
+      ff = Fraccion.new(f1.n * f2.n, f1.d * f2.d)
+      reducir(ff.n,ff.d)
+   end
+
+   def division(other)
+   	f1 = reducir(@n,@d)
+   	f2 = reducir(other.n, other.d)
+      ff = Fraccion.new(f1.n * f2.d, f1.d * f2.n)
+      reducir(ff.n,ff.d)
+   end
 end
+
+a = Fraccion.new(13,3)
+b = Fraccion.new(46,5)
+c = a.suma(b)
+puts c.to_s
+d = a.resta(b)
+puts d.to_s
+e = a.producto(b)
+puts e.to_s
+f = a.division(b)
+puts f.to_s
+

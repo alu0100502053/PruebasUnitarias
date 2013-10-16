26f4fdb86c3e4bd00be4800ef7970be777b3f4ea
Killian Jimenez <alu0100502053@ull.edu.es>
2013-10-16 10:12:38 +0100

    Cambios en racional.rb

--- racional.rb
+++ racional.rb
@@ -18,9 +18,7 @@ class Fraccion
    	Fraccion.new(n/mcd, d/mcd)
    end
 
-   def suma(other)
-   	f1 = reducir(@n,@d)
-   	f2 = reducir(other.n, other.d)
+   def sumayresta(other,f1,f2)
    	if f1.d != f2.d then
    		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
    		f1.n = mcm / f1.d * f1.n
@@ -28,6 +26,12 @@ class Fraccion
    		f2.n = mcm / f2.d * f2.n
    		f2.d = mcm
    	end
+   end
+
+   def suma(other)
+   	f1 = reducir(@n,@d)
+   	f2 = reducir(other.n, other.d)
+   	sumayresta(other,f1,f2)
    	ff = Fraccion.new(f1.n + f2.n, f1.d)
    	reducir(ff.n,ff.d)
    end
@@ -35,13 +39,7 @@ class Fraccion
    def resta(other)
    	f1 = reducir(@n,@d)
    	f2 = reducir(other.n, other.d)
-   	if f1.d != f2.d then
-   		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
-   		f1.n = mcm / f1.d * f1.n
-   		f1.d = mcm
-   		f2.n = mcm / f2.d * f2.n
-   		f2.d = mcm
-   	end
+   	sumayresta(other,f1,f2)
    	ff = Fraccion.new(f1.n - f2.n, f1.d)
    	reducir(ff.n,ff.d)
    end
@@ -59,6 +57,11 @@ class Fraccion
       ff = Fraccion.new(f1.n * f2.d, f1.d * f2.n)
       reducir(ff.n,ff.d)
    end
+
+   def ==(other)
+   	return @n == other.n && @d  == other.d if other.instance_of? Fraccion
+   	false
+   end
 end
 
 a = Fraccion.new(13,3)
@@ -71,4 +74,3 @@ e = a.producto(b)
 puts e.to_s
 f = a.division(b)
 puts f.to_s
-

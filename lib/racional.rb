f72053b433138b1ed6bb521f08118d23dc85114e
Killian Jimenez <alu0100502053@ull.edu.es>
2013-10-18 14:30:54 +0100

    Modificaciones de Ultima hora: simplificado todo y creado el racional.rb fuera de /lib/

--- lib/racional.rb
+++ lib/racional.rb
@@ -3,7 +3,7 @@
 require "./lib/gcd.rb"
 
 class Fraccion
-	attr_accessor :n, :d
+	attr_reader :n, :d
 	
 	def initialize(n,d)
       raise RuntimeError unless n.is_a? Integer and d.is_a? Integer
@@ -14,49 +14,31 @@ class Fraccion
       "#{@n}/#{@d}"
    end
 
-   def reducir(n,d)
-   	mcd = gcd(n,d)
-   	Fraccion.new(n/mcd, d/mcd)
-   end
-
-   def sumayresta(other,f1,f2)
-   	if f1.d != f2.d then
-   		mcm = (f1.d * f2.d) / gcd(f1.d,f2.d)
-   		f1.n = mcm / f1.d * f1.n
-   		f1.d = mcm
-   		f2.n = mcm / f2.d * f2.n
-   		f2.d = mcm
-   	end
+   def reducir(f)
+      mcd = gcd(f.n, f.d)
+      Fraccion.new(f.n/mcd, f.d/mcd)
    end
 
    def suma(other)
-   	f1 = reducir(@n,@d)
-   	f2 = reducir(other.n, other.d)
-   	sumayresta(other,f1,f2)
-   	ff = Fraccion.new(f1.n + f2.n, f1.d)
-   	reducir(ff.n,ff.d)
+      mcm = (@d * other.d)/gcd(@d, other.d)
+      f1 = Fraccion.new((mcm/@d) * @n, mcm)
+      f2 = Fraccion.new((mcm/other.d) * other.n, mcm)
+      reducir(Fraccion.new(f1.n + f2.n, mcm))
    end
 
    def resta(other)
-   	f1 = reducir(@n,@d)
-   	f2 = reducir(other.n, other.d)
-   	sumayresta(other,f1,f2)
-   	ff = Fraccion.new(f1.n - f2.n, f1.d)
-   	reducir(ff.n,ff.d)
+      mcm = (@d * other.d)/gcd(@d, other.d)
+      f1 = Fraccion.new((mcm/@d) * @n, mcm)
+      f2 = Fraccion.new((mcm/other.d) * other.n, mcm)
+      reducir(Fraccion.new(f1.n - f2.n, mcm))
    end
 
    def producto(other)
-      f1 = reducir(@n,@d)
-   	f2 = reducir(other.n, other.d)
-      ff = Fraccion.new(f1.n * f2.n, f1.d * f2.d)
-      reducir(ff.n,ff.d)
+      reducir(Fraccion.new(@n * other.n, @d * other.d))
    end
 
    def division(other)
-   	f1 = reducir(@n,@d)
-   	f2 = reducir(other.n, other.d)
-      ff = Fraccion.new(f1.n * f2.d, f1.d * f2.n)
-      reducir(ff.n,ff.d)
+      reducir(Fraccion.new(@n * other.d, @d * other.n))
    end
 
    def ==(other)
@@ -68,10 +50,10 @@ end
 # a = Fraccion.new(13,3)
 # b = Fraccion.new(46,5)
 # c = a.suma(b)
-# puts c.to_s
+# puts "Suma: " + c.to_s
 # d = a.resta(b)
-# puts d.to_s
+# puts "Resta: " + d.to_s
 # e = a.producto(b)
-# puts e.to_s
+# puts "Producto: " + e.to_s
 # f = a.division(b)
-# puts f.to_s
+# puts "Division: " + f.to_s
